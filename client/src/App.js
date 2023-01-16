b13425568c1891bcd1f57905910cb295537803a8
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-01-16 15:28:11 +0100

    installed dependencies

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+
+function App() {
+  const user = true
+  //  useSelector((state) => state.authReducer.authData);
+  return (
+    <div
+     
+    >
+      
+    </div>
+  );
+}
+
+export default App;
