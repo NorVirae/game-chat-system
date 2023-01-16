6793f246386873f97de5015b4a3ee4747f3f3141
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-01-16 16:11:22 +0100

    added posts in home and integrated reducers

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostSide from "../components/PostSide/PostSide";
+
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="Home">
+      <PostSide />
+    </div>
+  );
+};
+
+export default Home;
