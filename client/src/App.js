61b2b96b6f7cf87a09fccd82a7c132fff578e4db
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-01-16 16:27:34 +0100

    added routes to home and sign in sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth/Auth";
+import { useSelector } from "react-redux";
 
 function App() {
-  const user = true;
-  //  useSelector((state) => state.authReducer.authData);
-  return <div></div>;
+  const user = useSelector((state) => state.authReducer.authData);
+  return (
+    <div
+      className="App"
+      style={{
+        height:
+          window.location.href === "http://localhost:3000/chat"
+            ? "calc(100vh - 2rem)"
+            : "auto",
+      }}
+    >
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+        
+        
+
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
